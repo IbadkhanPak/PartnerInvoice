04ef345b66aaff55d1a539c4376163d233530ca4
ibad <irehman@folio3.com>
2025-04-12 16:01:56 +0500

    Create invoice logic

--- app/code/Ibad/PartnerInvoiceShipment/Observer/Frontend/Sales/OrderPlaceAfter.php
+++ app/code/Ibad/PartnerInvoiceShipment/Observer/Frontend/Sales/OrderPlaceAfter.php
@@ -3,17 +3,19 @@ declare(strict_types=1);
 
 namespace Ibad\PartnerInvoiceShipment\Observer\Frontend\Sales;
 
-use \Magento\Framework\Event\Observer;
-use \Magento\Framework\Event\ObserverInterface;
-use \Psr\Log\LoggerInterface;
-use  \Magento\Sales\Model\Order\ShipmentFactory;
-use \Magento\Sales\Model\Service\ShipmentService;
-use \Magento\Sales\Model\Service\InvoiceService;
-use \Magento\Sales\Model\Order\Shipment\ItemFactory;
-use \Magento\Sales\Api\ShipmentRepositoryInterface;
-use \Magento\Framework\DB\Transaction;
-use \Magento\Sales\Model\Convert\Order;
-use tests\unit\Magento\FunctionalTestFramework\Util\Validation\NameValidationUtilTest;
+use Exception;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Psr\Log\LoggerInterface;
+use  Magento\Sales\Model\Order\ShipmentFactory;
+use Magento\Sales\Model\Service\ShipmentService;
+use Magento\Sales\Model\Service\InvoiceService;
+use Magento\Sales\Model\Order\Shipment\ItemFactory;
+use Magento\Sales\Api\ShipmentRepositoryInterface;
+use Magento\Framework\DB\Transaction;
+use Magento\Sales\Model\Convert\Order;
+use Magento\Framework\DB\TransactionFactory;
+
 
 class OrderPlaceAfter implements ObserverInterface
 {
@@ -28,7 +30,7 @@ class OrderPlaceAfter implements ObserverInterface
     /**
      * @var
      */
-    protected  $invoiceService;
+    protected $invoiceService;
 
     /**
      * @var ItemFactory
@@ -41,31 +43,40 @@ class OrderPlaceAfter implements ObserverInterface
     protected $shipmentRepository;
 
     /**
-     * @var Transaction
+     * @var TransactionFactory
      */
     protected $transaction;
 
     protected $convertOrder;
+
     /**
      * @param LoggerInterface $logger
      * @param ShipmentFactory $shipmentFactory
      * @param ItemFactory $shipmentItemFactory
-     * @param ShipmentRepository $shipmentRepository
+     * @param ShipmentRepositoryInterface $shipmentRepository
+     * @param Transaction $transaction
+     * @param Order $convertOrder
+     * @param InvoiceService $invoiceService
+     * @param TransactionFactory $transactionFactory
      */
     public function __construct(
-        LoggerInterface $logger,
-        ShipmentFactory $shipmentFactory,
-        ItemFactory $shipmentItemFactory,
+        LoggerInterface             $logger,
+        ShipmentFactory             $shipmentFactory,
+        ItemFactory                 $shipmentItemFactory,
         ShipmentRepositoryInterface $shipmentRepository,
-        Transaction $transaction,
-        Order $convertOrder
-    ){
+        Transaction                 $transaction,
+        Order                       $convertOrder,
+        InvoiceService              $invoiceService,
+        TransactionFactory          $transactionFactory
+    )
+    {
         $this->logger = $logger;
         $this->shipmentFactory = $shipmentFactory;
         $this->shipmentItemFactory = $shipmentItemFactory;
         $this->shipmentRepository = $shipmentRepository;
-        $this->transaction = $transaction;
+        $this->transaction = $transactionFactory;
         $this->convertOrder = $convertOrder;
+        $this->invoiceService = $invoiceService;
     }
 
     /**
@@ -80,18 +91,18 @@ class OrderPlaceAfter implements ObserverInterface
         if (isset($_COOKIE['partner'])) {
 
             //set parter name in order
-            $this->setPartnerName($_COOKIE['partner'],$order);
+            $this->setPartnerName($_COOKIE['partner'], $order);
 
             // Get all visible items from the order
             $items = $order->getAllVisibleItems();
             $itemCount = count($items);
             $numShipments = ($itemCount > 1) ? 2 : 1;
-            $chunks = array_chunk($items, (int) ceil($itemCount / $numShipments));
+            $chunks = array_chunk($items, (int)ceil($itemCount / $numShipments));
 
-            $this->logger->debug('Chunkssss '. print_r($chunks,true));
+            $this->logger->debug('Chunkssss ' . print_r($chunks, true));
 
             foreach ($chunks as $chunk) {
-                $this->logger->debug('Chunk '. print_r($chunk,true));
+                $this->logger->debug('Chunk ' . print_r($chunk, true));
                 $shipment = $this->shipmentFactory->create($order);
                 foreach ($chunk as $item) {
 
@@ -111,14 +122,34 @@ class OrderPlaceAfter implements ObserverInterface
                 $shipment->register();
                 $shipment->getOrder()->setIsInProcess(true);
 
-                try {
-                    $shipment->save();
-                    $shipment->getOrder()->save();
-                    $this->logger->debug('Shipment created');
-                } catch (\Exception $e) {
-                    $this->logger->error('Shipment creation failed: ' . $e->getMessage());
+                // Create Invoice
+
+                $invoiceItems = [];
+                foreach ($chunk as $item) {
+                    $this->logger->debug('Item Id '.$item->getId());
+                    $this->logger->debug('Item Qty Invoice '.$item->getQtyToInvoice());
+
+                    $invoiceItems[$item->getId()] = $item->getQtyToInvoice();
                 }
 
+                $invoice = $this->invoiceService->prepareInvoice($order, $invoiceItems);
+                $invoice->register();
+                $invoice->getOrder()->setIsInProcess(true);
+
+
+                try {
+                    $transactionSave = $this->transaction->create()
+                        ->addObject($shipment)
+                        ->addObject($invoice)
+                        ->addObject($shipment->getOrder());
+
+                    $transactionSave->save();
+
+
+                    $this->logger->debug('Shipment created');
+                } catch (Exception $e) {
+                    $this->logger->error('Shipment creation failed: ' . $e->getMessage());
+                }
             }
         }
     }
@@ -128,9 +159,9 @@ class OrderPlaceAfter implements ObserverInterface
      * @param $order
      * @return void
      */
-    public function setPartnerName($partner,$order)
+    public function setPartnerName($partner, $order)
     {
-        if(!empty($partner)){
+        if (!empty($partner)) {
             $order->setData('partner_name', $_COOKIE['partner']);
             $this->logger->debug('Set Partner: ' . $_COOKIE['partner']);
         }
